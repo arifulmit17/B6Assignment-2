25f89c2fa0d20eec78601a5dccb317fe9178cc06
Ariful <arifulmit17@gmail.com>
2025-12-08 14:47:33 +0600

    added admin auth to creating vehicle and readme file

---
+++ README.md
@@ -0,0 +1,20 @@
+Project name: Vehicle booking system
+
+Live url:
+
+Features:
+1. Roll based user authentication.
+2. Vehicle booking facility.
+3. Booking cancellation at any time before start.
+4. Check booking availability.
+
+
+Technology Stack: 
+1. Node.js
+2. Express.js
+3. JWT
+4. Postgresql
+
+Setup and usage instruction:
+
+1.after deployment hit the available api with postman for desired tasks.
--- src/modules/vehicles/vehicles.route.ts
+++ src/modules/vehicles/vehicles.route.ts
@@ -8,8 +8,8 @@ import auth from "../../middleware/auth";
 
 const router = Router();
 
-router.post('/vehicles',vehiclesController.createVehicles);
-router.get('/vehicles',auth("admin"),vehiclesController.getAllVehicles);
+router.post('/vehicles',auth("admin"),vehiclesController.createVehicles);
+router.get('/vehicles',vehiclesController.getAllVehicles);
 router.get('/vehicles/:vehicleId',vehiclesController.getVehicle);
 router.put('/vehicles/:vehicleId',auth("admin"),vehiclesController.updateVehicle);
 router.delete('/vehicles/:vehicleId',auth("admin"),vehiclesController.deleteVehicle);
